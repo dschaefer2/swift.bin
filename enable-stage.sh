391f2badda237d7d1ce467452bb942bdc56467cf
Doug Schaefer <dschaefer2@apple.com>
2026-06-13 11:40:34 -0400

    latest updates

--- enable-stage.sh
+++ enable-stage.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
-defaults write com.apple.dt.Xcode IDEPackagePrebuiltsDownloadURL file:///Users/dschaefer2/swift/stage
-defaults write com.apple.dt.Xcode IDEPackagePrebuiltsRootCert /Users/dschaefer2/swift/swiftpm/Fixtures/Signing/Certificates/TestRootCA.cer
+defaults write com.apple.dt.Xcode IDEPackagePrebuiltsDownloadURL file:///Users/dschaefer2/swift/swiftpm/stage
+defaults write com.apple.dt.Xcode IDEPackagePrebuiltsRootCert /Users/dschaefer2/swift/swiftpm/stage/TestRootCA.cer
